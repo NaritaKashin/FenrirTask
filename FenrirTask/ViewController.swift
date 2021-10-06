8d245770510d4b7d03afb08e2665a34cdfe881ad
SS S <sss@ssnomacbook-pro.local>
2021-10-07 05:19:53 +0900

    Initial Commit

---
+++ FenrirTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FenrirTask
+//
+//  Created by SS S on 2021/10/07.
+//  Copyright © 2021年 成田華心. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FenrirTaskTests/FenrirTaskTests.swift
@@ -0,0 +1,34 @@
+//
+//  FenrirTaskTests.swift
+//  FenrirTaskTests
+//
+//  Created by SS S on 2021/10/07.
+//  Copyright © 2021年 成田華心. All rights reserved.
+//
+
+import XCTest
+@testable import FenrirTask
+
+class FenrirTaskTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
